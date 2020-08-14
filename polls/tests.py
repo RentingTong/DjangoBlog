f11a8dcb421c70d9feb9a029ef55dfcbd4eac25e
tongt1 <Tara.Tong@emc.com>
2020-08-14 17:58:22 +0800

    08141757

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,35 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+
+    def test_001_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is in the future.
+        :return: boolean
+        """
+        time = timezone.now() + datetime.timedelta(days=1)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_002_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is older than 30 days.
+        :return: boolean
+        """
+        time = timezone.now() - datetime.timedelta(days=30, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_003_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is within 30 days.
+        :return: boolean
+        """
+        time = timezone.now() - datetime.timedelta(days=29, hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
