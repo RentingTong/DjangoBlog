18c442a2fcb448d66f0e2375685a5a477edb59f0
狐晚 <trttong@outlook.com>
2020-08-17 22:01:18 +0800

    blog

---
+++ blog/templates/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@date: 2020/08/
+
+@author: Tara
+
+@description:
+
+"""
+
---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@date: 2020/08/17
+
+@author: Tara
+
+@description:
+
+"""
+from django.urls import path
+from . import views
+
+
+app_name = "blog"
+
+urlpatterns = [
+    path(),
+    path()
+]
+
+
